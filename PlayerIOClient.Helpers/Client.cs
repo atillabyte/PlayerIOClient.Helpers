26ba810c87f0c93c4a7796989e07f7a12c146f67
Atilla Lonny <atillabyte@null.net>
2016-09-12 23:13:53 -0400

    Updates #5
    [+] Client.SetToken()
    [+] Client.GetToken()

---
+++ PlayerIOClient.Helpers/Client.cs
@@ -0,0 +1,10 @@
+﻿using PlayerIOClient.Helpers.Helpers;
+
+namespace PlayerIOClient.Helpers
+{
+    public static class ClientExtensions
+    {
+        public static void SetToken(this Client client, string token) => PropertyHelper.SetPrivatePropertyValue(Reflection.GetFieldValue(client, "channel"), "Token", token);
+        public static string GetToken(this Client client) => PropertyHelper.GetPrivatePropertyValue<string>(Reflection.GetFieldValue(client, "channel"), "Token");
+    }
+}
